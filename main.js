40c0fb7a64ee8db18c2026c782319f2a915bbda5
musquitinha <isabela.thome@escola.pr.gov.br>
2026-08-13 15:04:38 +0000

    Aula 04

--- main.js
+++ main.js
@@ -50,6 +50,16 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
+function mostraAlternativas() {}
 
 mostraPergunta();
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
